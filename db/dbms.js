f80c14e7709f6f85ad365af8f988fa8daf966d7b
Tauhoo <tauhoo_ice@hotmail.com>
2021-05-20 19:02:03 +0700

    add link to newCargo button

--- db/dbms.js
+++ db/dbms.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '123456',
   database: 'shop',
   port: '3306',
 })
